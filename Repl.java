03880c1a6b287c91a7e2869661c42d6e0cbc76ee
WilliamRagstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-06-10 17:05:54 +0200

    Update Repl.java

--- Repl.java
+++ Repl.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
+import java.util.NoSuchElementException;
 
 public class Repl {
     public static void main(String[] args) throws Exception {
         Interpreter i = new Interpreter();
-
         Scanner sc = new Scanner(System.in);
-
         for (;;) {
             try {
                 System.out.print("> ");
                 String expr = sc.nextLine();
                 i.execute(expr);
-            } catch (java.util.NoSuchElementException e) {
+            } catch (NoSuchElementException e) {
                 sc.close();
                 break;
             } catch (Exception e) {
