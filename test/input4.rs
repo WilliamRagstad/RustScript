f0b4f0dcc1d144d0708190608d04f42e3874f962
WilliamRagstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-06-19 12:33:02 +0200

    Add tests for multiline expressions
    Make tokenizer ignore whitespace if inside an expression

--- test/input4.rs
+++ test/input4.rs
@@ -1,3 +1,14 @@
+let R1 = ^reverse([let d = "ThisIsNice", let d = substr(d, 6, 10), d + " weather"]);
+
+let R2 = ^reverse([
+    let d = println("This"),
+    let d = "is",
+    println(d),
+    let d = ^d - 9,
+    println(d),
+    println("test")
+])
+
 let getBool = fn(s) => if (^lower(s) == 't') then substr(s, 0, 4) else substr(s, 0, 5)
 println(getBool("truethis"))
 println(getBool("falsethis"))
