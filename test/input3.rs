df8949974defeaf89b55942729179c2b3b9bbec8
WilliamRagstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-06-19 12:24:08 +0200

    Use has builtin

--- test/input3.rs
+++ test/input3.rs
@@ -1,4 +1,4 @@
 let text = input("Enter number: ")
 let val = parseVal(text)
 println(typeof(val))
-if typeof(val) == "Val" then println(val * 2) else println("Could not parse input number!")
+if has(val) then println(val * 2) else println("Could not parse input number!")
