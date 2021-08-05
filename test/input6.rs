30c8f097b7bc28a4484d7a4c126599e794fa9256
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-08-05 17:25:06 +0200

    Fix test

--- test/input6.rs
+++ test/input6.rs
@@ -1,5 +1,5 @@
 // Test case expressions
 let text = input("Enter number: ")
 match parseVal(text)
-	got [val, val2] and has(val) then println(val * 2)
+	got val and has(val) then println(val * 2)
 	got _ then println("Could not parse input number!")
