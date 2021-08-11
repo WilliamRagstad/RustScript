30bf03d90cc8c5d8039d9d3f334bcb423e33d4e8
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-08-11 16:13:08 +0200

    Update input7.rs

--- test/input7.rs
+++ test/input7.rs
@@ -1,11 +1,11 @@
-let blockFn = fn() => {
-	let x = 1;
+// Code blocks
+let blockFn = fn(x) => {
 	let y = 2;
 	let z = 3;
 	x + y + z;
 }
 
-println(blockFn());
+println(blockFn(5));
 
 {
 	// Block scoped variables
