63e5455321b4f9bffd8a9d800707bb25bd1ff944
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-08-16 15:43:53 +0200

    Update tests

--- test/input9.2.rs
+++ test/input9.2.rs
@@ -1,3 +1,3 @@
 imp first from ".\\input9.1.rs";
 
-println("imported nested from external file:", first.second.third.nested)
+println("imported nested from external file:", first.second.third.nested);
